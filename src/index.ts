bc4d4d4a93635c82d5a99079c33e6ca8401b7699
MackenzieCameron7 <54957783+MackenzieCameron7@users.noreply.github.com>
2025-03-01 23:09:46 -0500

    Modified compareImages function
    Modified compareImages function to compare an image to 16 other images/sections within a1920 x 1080  screenshot.
    
    Observations:
    - src/index.ts line 122-128. takes alll attachments from all messages created within same session, instead of just the most recent. Only uses most recent anyway.
    - I do not account for things such as: larger or lower screen resolution than 1920 x 1080 or for having "Realm of the Mad God" in windowed mode. Don't understand how some of my friends (degenerates) play a perma death bullet hell in a smaller window, while playing other games....

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js';
-import { createCanvas, loadImage } from 'canvas';
+import { createCanvas, Image, loadImage } from 'canvas';
 import fetch from "node-fetch";
 import pixelmatch from 'pixelmatch';
 
@@ -22,73 +22,88 @@ interface myImageObject {
   title: string;
 }
 
-////////////// Saving this code here to modify/add to compare imgae functionality to check the 16 inventory
-///////////// slots of a screenshot no matter the size... 
-
-// // Screenshot height and width
-// const screenWidth = img1.width
-// const screenHeight = img1.height
-
-// // Spots of the incoming screenshot based on where inventory slots sit in relation to screenshot
-// // width and height in percentage% form. refer to "image png" for screenshot reference
-// const spots = [
-//   //First inventory row
-//   {x: screenWidth * 0.84, y: screenHeight * 0.67},
-//   {x: screenWidth * 0.88, y: screenHeight * 0.67},
-//   {x: screenWidth * 0.92, y: screenHeight * 0.67},
-//   {x: screenWidth * 0.96, y: screenHeight * 0.67},
-//   //Second
-//   {x: screenWidth * 0.84, y: screenHeight * 0.75},
-//   {x: screenWidth * 0.88, y: screenHeight * 0.75},
-//   {x: screenWidth * 0.92, y: screenHeight * 0.75},
-//   {x: screenWidth * 0.96, y: screenHeight * 0.75},
-//   //Third
-//   {x: screenWidth * 0.84, y: screenHeight * 0.88},
-//   {x: screenWidth * 0.88, y: screenHeight * 0.88},
-//   {x: screenWidth * 0.92, y: screenHeight * 0.88},
-//   {x: screenWidth * 0.96, y: screenHeight * 0.88},
-//   //Fourth
-//   {x: screenWidth * 0.84, y: screenHeight * 0.96},
-//   {x: screenWidth * 0.88, y: screenHeight * 0.96},
-//   {x: screenWidth * 0.92, y: screenHeight * 0.96},
-//   {x: screenWidth * 0.96, y: screenHeight * 0.96},
-// ]
-
 // Function to compare images using html element canvases. lhs is icoming from user. rhs is coming from database
-async function compareImages(url1: string, url2: string):  Promise<number> {
+async function compareImages(screenshotUrl: string, dbUrl: string):  Promise<number[]> {
+  
+  //Array of numbers of pixel difference comparison
+  const diffArray: number[] = []
 
   // Loading images from the url extracted from image object
-  const [img1, img2] = await Promise.all([loadImage(url1), loadImage(url2)])
+  const [img1, img2] = await Promise.all([loadImage(screenshotUrl), loadImage(dbUrl)])
   // TESTING
   console.log("Images loaded successfully")
 
   // Checking spots from screenshot based on DIMENSIONS of images from db
-  const width = img2.width
-  const height = img2.height
+  const width1 = img1.width
+  const height1 = img1.height
+  const width2 = img2.width
+  const height2 = img2.height
 
   // Creation of Canvases
-  const canvas1 = createCanvas(width, height)
-  const canvas2 = createCanvas(width, height)
+  const canvas1 = createCanvas(width1, height1)
+  const canvas2 = createCanvas(width2, height2)
   const context1 = canvas1.getContext("2d")
   const context2 = canvas2.getContext("2d")
 
   // Draw the images onto canvases 
-  context1.drawImage(img1, 0, 0, width, height)
-  context2.drawImage(img2, 0, 0, width, height)
-
-  // Getting pixel data from images
-  const imgData1 = context1.getImageData(0,0, width, height)
-  const imgData2 = context2.getImageData(0,0, width, height)
+  context1.drawImage(img1, 0, 0, width1, height1)
+  context2.drawImage(img2, 0, 0, width2, height2)
 
   // Creating a diff canvas
-  const diffCanvas = createCanvas(width, height)
+  const diffCanvas = createCanvas(width1, height1)
   const diffContext = diffCanvas.getContext("2d")
-  const diff = diffContext.createImageData(width, height)
 
-  // Compare Images
-  const numDiffPixels = pixelmatch(imgData1.data, imgData2.data, diff.data, width, height, {threshold: 0.1})
+  // Screenshot height and width
+  const screenWidth = img1.width
+  const screenHeight = img1.height
 
-  return numDiffPixels
+  // 16 positions of screenshots with a resolution of 1920 x 1080
+  // FUTURE: Could caculate coordinates if png sent to chat != 1920 x 1080 ex x: screenWidth*0.84
+  // Account for windowed mode and screen sizes with resolutions larger/smaller than 1920 x 1080
+  const spots = [
+   //First inventory row
+   {x: 1580, y: 688},
+   {x: 1663, y: 688},
+   {x: 1746, y: 688},
+   {x: 1829, y: 688},
+   //Second
+   {x: 1580, y: 771},
+   {x: 1663, y: 771},
+   {x: 1746, y: 771},
+   {x: 1829, y: 771},
+   //Third
+   {x: 1580, y: 913},
+   {x: 1663, y: 913},
+   {x: 1746, y: 913},
+   {x: 1829, y: 913},
+   //Fourth
+   {x: 1580, y: 996},
+   {x: 1663, y: 996},
+   {x: 1746, y: 996},
+   {x: 1829, y: 996},
+  ]   
+  
+  // Compare each inventory slot in screenshot to rhs
+  for (const {x, y} of spots){
+
+    // Getting pixel data from images
+    const imgData1 = context1.getImageData(x, y, width2, height2)
+    const imgData2 = context2.getImageData(0,0, width2, height2)
+    const diff = diffContext.createImageData(width2, height2)
+
+    const numDiffPixels = pixelmatch(imgData1.data, imgData2.data, diff.data, width2, height2, { threshold: 0.1 })
+
+    //TESTING
+    console.log(`Slot first pixel: R${imgData1.data[0]}, G${imgData1.data[1]}, B${imgData1.data[2]}`);
+    console.log(`Item first pixel: R${imgData2.data[0]}, G${imgData2.data[1]}, B${imgData2.data[2]}`);
+
+    //TESTING
+    console.log(numDiffPixels.toString())
+
+    diffArray.push(numDiffPixels)
+  }
+
+  return diffArray
 }
 
 
@@ -125,10 +140,8 @@ client.on(Events.MessageCreate, async (message) => {
       const imgUrl: string = img["url"]
 
       // Comparing images returning a number
-      //TESTING
-      await compareImages(imgUrl, "src\\Candy Ring(Shiny).png")
+      await compareImages(imgUrl, "src\\Glife.png")
     })
-
   }
 });
 
